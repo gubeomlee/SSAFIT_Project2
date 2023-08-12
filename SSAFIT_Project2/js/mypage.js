996f6794038ebb992ac257d71175937c1f6b9720
gubeomlee <29tigerhg@gmail.com>
2023-08-12 17:00:25 +0900

    마무리

--- SSAFIT_Project2/js/mypage.js
+++ SSAFIT_Project2/js/mypage.js
@@ -1,36 +1,80 @@
 let userInfo = document.querySelectorAll(".userInfo");
 let like = document.querySelectorAll(".like");
+let likeVideo = document.querySelectorAll(".like iframe");
 let myReview = document.querySelectorAll(".myReview");
 let fallow = document.querySelectorAll(".fallow");
+let fallowList = document.querySelectorAll(".fallow div");
+
+userInfo[1].style.display = "block";
+likeVideo.forEach((video) => {
+  video.style.display = "none";
+});
+myReview[1].style.display = "none";
+fallowList.forEach((f) => {
+  f.style.display = "none";
+});
+userInfo[0].classList.add("fw-bold");
 
 userInfo[0].addEventListener("click", () => {
   userInfo[1].style.display = "block";
-  like[1].style.display = "none";
+  likeVideo.forEach((video) => {
+    video.style.display = "none";
+  });
   myReview[1].style.display = "none";
-  fallow[1].style.display = "none";
-  console.log(1);
+  fallowList.forEach((f) => {
+    f.style.display = "none";
+  });
+
+  userInfo[0].classList.add("fw-bold");
+  like[0].classList.remove("fw-bold");
+  myReview[0].classList.remove("fw-bold");
+  fallow[0].classList.remove("fw-bold");
 });
 
 like[0].addEventListener("click", () => {
   userInfo[1].style.display = "none";
-  like[1].style.display = "block";
+  likeVideo.forEach((video) => {
+    video.style.display = "block";
+  });
   myReview[1].style.display = "none";
-  fallow[1].style.display = "none";
-  console.log(1);
+  fallowList.forEach((f) => {
+    f.style.display = "none";
+  });
+
+  like[0].classList.add("fw-bold");
+  userInfo[0].classList.remove("fw-bold");
+  myReview[0].classList.remove("fw-bold");
+  fallow[0].classList.remove("fw-bold");
 });
 
 myReview[0].addEventListener("click", () => {
   userInfo[1].style.display = "none";
-  like[1].style.display = "none";
+  likeVideo.forEach((video) => {
+    video.style.display = "none";
+  });
   myReview[1].style.display = "block";
-  fallow[1].style.display = "none";
-  console.log(1);
+  fallowList.forEach((f) => {
+    f.style.display = "none";
+  });
+
+  myReview[0].classList.add("fw-bold");
+  userInfo[0].classList.remove("fw-bold");
+  like[0].classList.remove("fw-bold");
+  fallow[0].classList.remove("fw-bold");
 });
 
 fallow[0].addEventListener("click", () => {
   userInfo[1].style.display = "none";
-  like[1].style.display = "none";
+  likeVideo.forEach((video) => {
+    video.style.display = "none";
+  });
   myReview[1].style.display = "none";
-  fallow[1].style.display = "block";
-  console.log(1);
+  fallowList.forEach((f) => {
+    f.style.display = "block";
+  });
+
+  fallow[0].classList.add("fw-bold");
+  userInfo[0].classList.remove("fw-bold");
+  like[0].classList.remove("fw-bold");
+  myReview[0].classList.remove("fw-bold");
 });
